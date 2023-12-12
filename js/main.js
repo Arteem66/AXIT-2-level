b71936ef0a0f4cd4e303b01108ca077a5719ec45
SaldNevo <artemkorobov4119@gmail.com>
2023-12-12 17:11:58 +0300

    crooked javascript tabs

--- js/main.js
+++ js/main.js
@@ -1,46 +1,90 @@
-const wrapper = document.querySelector(".wrapper")
-const burger = document.querySelector(".burger")
+// ==================BURGER-MENU=============
+// ==================BURGER-MENU=============
+
+const wrapper = document.querySelector('.wrapper')
+const burger = document.querySelector('.burger')
 const body = document.body
-const navList = document.querySelector(".nav__list")
-const inner = document.querySelector(".header__inner")
+const navList = document.querySelector('.nav__list')
+const inner = document.querySelector('.header__inner')
 const pageWidth = document.documentElement.scrollWidth
 
-burger.addEventListener("click", openBurger)
+burger.addEventListener('click', openBurger)
 
-if (pageWidth >= 800){
+if (pageWidth >= 800) {
 	burger.classList.add('hidden')
 	navList.classList.remove('hidden')
-}else{
+} else {
 	burger.classList.remove('hidden')
 	navList.classList.add('hidden')
 }
 
 window.addEventListener('resize', function () {
 	if (window.matchMedia('(min-width: 800px)').matches) {
-		burger.classList.add("hidden")
-		navList.classList.remove("hidden")
+		burger.classList.add('hidden')
+		navList.classList.remove('hidden')
 	} else {
 		burger.classList.remove('hidden')
 		navList.classList.add('hidden')
 	}
 })
 
-function openBurger (){
-	wrapper.classList.toggle("hidden")
-	body.classList.toggle("noscroll")
-	inner.classList.toggle("inner__burger")
-	navList.classList.toggle("hidden")
-	navList.classList.toggle("nav__list-burger")
+function openBurger() {
+	wrapper.classList.toggle('hidden')
+	body.classList.toggle('noscroll')
+	inner.classList.toggle('inner__burger')
+	navList.classList.toggle('hidden')
+	navList.classList.toggle('nav__list-burger')
 
-	function closeBurgerLink(){
-			wrapper.classList.add('hidden')
-			body.classList.remove('noscroll')
-			inner.classList.remove('inner__burger')
-			navList.classList.add('hidden')
-			navList.classList.remove('nav__list-burger')
+	navList.addEventListener('click', closeBurgerLink)
+
+	function closeBurgerLink() {
+		wrapper.classList.add('hidden')
+		body.classList.remove('noscroll')
+		inner.classList.remove('inner__burger')
+		navList.classList.add('hidden')
+		navList.classList.remove('nav__list-burger')
 	}
-
-	navList.addEventListener("click", closeBurgerLink)
 }
 
-const button = document.querySelector(".descr__button")
+
+
+
+let tabButton = document.querySelectorAll('.descr__button')
+let slides = document.querySelectorAll('.slide__text')
+
+console.log(tabButton);
+
+Array.from(tabButton).forEach(b => {
+	b.addEventListener('click', function () {
+
+		if (b == tabButton[0]){
+			tabButton.forEach(tabButton => {
+				tabButton.classList.remove('descr__button-active')
+			})
+			tabButton[0].classList.add('descr__button-active')
+			slides.forEach(slide =>{
+				slide.classList.remove('slide__text-active')
+			})
+			slides[0].classList.add('slide__text-active')
+		}else if (b == tabButton[1]) {
+			tabButton.forEach(tabButton => {
+				tabButton.classList.remove('descr__button-active')
+			})
+			tabButton[1].classList.add('descr__button-active')
+			slides.forEach(slide => {
+				slide.classList.remove('slide__text-active')
+			})
+			slides[1].classList.add('slide__text-active')
+		}else{
+			tabButton.forEach(tabButton => {
+				tabButton.classList.remove('descr__button-active')
+			})
+			tabButton[2].classList.add('descr__button-active')
+			slides.forEach(slide => {
+				slide.classList.remove('slide__text-active')
+			})
+			slides[2].classList.add('slide__text-active')
+		}
+	})
+})
+
